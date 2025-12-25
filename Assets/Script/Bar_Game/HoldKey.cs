47b4217b897dc432e122728480d66aba9c7d62ab
つけものの化身 <show64.ishi@gmail.com>
2025-12-25 15:44:40 +0900

    バーゲームの完成

--- Assets/Script/Bar_Game/HoldKey.cs
+++ Assets/Script/Bar_Game/HoldKey.cs
@@ -6,15 +6,40 @@ using UnityEngine.SceneManagement;
 
 public class HoldKey : MonoBehaviour
 {
+    private bool CanHold = true;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+            {
+                CanHold = true;
+            }
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Target")
         {
+            if(CanHold == false)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+            else
             if(!Input.GetKey(KeyCode.S))
             {
                 SceneManager.LoadScene("GameOver");
             }
         }
     }
+    
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Target")
+        {
+            CanHold = false;
+        }
+    }
+
+    
 }
